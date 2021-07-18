03eaf097a8f054dc85b96a9f30118a4eafe51ca8
RoderickChan <ch22166@163.com>
2021-07-18 23:50:34 +0800

    add author and email in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="pwncli",
     version="1.0",
+    author="roderick chan",
+    author_email="ch22166@163.com",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["click", "pwntools"],
