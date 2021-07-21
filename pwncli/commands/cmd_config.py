9a490267be6c45198da2d820c9b1e99d800ffda0
RoderickChan <ch22166@163.com>
2021-07-22 00:02:08 +0800

    add config set subcommand

--- pwncli/commands/cmd_config.py
+++ pwncli/commands/cmd_config.py
@@ -1,12 +1,19 @@
 import click
 import sys
-from pwncli.cli import pass_environ
+from pwncli.cli import pass_environ, AliasedGroup
 
 
-@click.command(name='config', short_help="Get or set something about config data.")
+@click.command(cls=AliasedGroup, name='config', short_help="Get or set something about config data.")
 @click.option('-l', '--list', type=str, default='all', required=False, show_default=True, help="List config data by section name.")
 @click.option('-v', '--verbose', is_flag=True, show_default=True, help="Show more info or not.")
 @pass_environ
 def cli(ctx, list, verbose):
     print("config...", verbose)
+    pass
+
+
+@cli.command(name="set", short_help="Set config data.")
+@pass_environ
+def set_config(ctx):
+    print("set conifg...")
     pass
