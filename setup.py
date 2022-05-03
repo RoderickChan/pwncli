48b1c8bc3ea6f18d22b14d28a3adc3c215ff39f3
RoderickChan <ch22166@163.com>
2022-05-03 15:12:20 +0800

    publish 1.2

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="pwncli",
-    version="1.1",
+    version="1.2",
     author="roderick chan",
     author_email="ch22166@163.com",
     description="pwncli, do pwn quickly.",
-    long_description="pwncli is a tool for pwner, make doing pwn quickly and more effeciently.",
+    long_description="pwncli is a tool for pwner, let you pwn quickly and more effeciently.",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "pwntools", "ropper", "lief"],
+    install_requires=["click", "pwntools", "ropper", "lief", "ROPgadget"],
     entry_points="""
         [console_scripts]
         pwncli=pwncli.cli:cli
