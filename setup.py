48a852632480802c2b6284119b40e5d43673025a
RoderickChan <ch22166@163.com>
2022-09-26 00:02:29 +0800

    publish pwncli 1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pwncli",
-    version="1.2",
+    version="1.3",
     author="roderick chan",
     author_email="ch22166@163.com",
     description="pwncli, do pwn quickly.",
